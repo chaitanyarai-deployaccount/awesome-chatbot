d070b2948b2dae1b07ffa46c464f6dde5d4383b2
chaitanyarai-deployaccount <147305573+chaitanyarai-deployaccount@users.noreply.github.com>
2023-12-28 21:05:10 +0530

    Update Intro.tsx

--- src/Components/Chat/Intro.tsx
+++ src/Components/Chat/Intro.tsx
@@ -4,13 +4,13 @@ const Intro = () => {
             <div className="text-center text-4xl font-bold text-brand ">Welcome to Code Llama on Akash</div>
             <div className="text-center text-lg text-gray-800 ">
                 <div className="mb-8">
-                    Discover the Power of Code llama on Akash Network powered by codellama/CodeLlama-34b-Instruct-hf model
+                    Discover the Power of Genrative AI on Akash Network powered by Meta code-llama
                 </div>
             </div>
             <div className="text-center text-gray-500 ">
                 <div className="mb-2">Ready to embark on a conversation?</div>
                 <div className="mb-2">
-        
+                        
                 </div>
                 <div className="mb-2">
                
